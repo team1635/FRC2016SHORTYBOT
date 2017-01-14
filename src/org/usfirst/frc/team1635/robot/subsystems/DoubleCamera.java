e75cc286b440dac8df8b5eef0df7885a003c6178
ljw980105 <ljw498043957>
2017-01-14 14:28:10 -0500

    Added Vision Processing

--- src/org/usfirst/frc/team1635/robot/subsystems/DoubleCamera.java
+++ src/org/usfirst/frc/team1635/robot/subsystems/DoubleCamera.java
@@ -1,8 +1,17 @@
 package org.usfirst.frc.team1635.robot.subsystems;
 
 
-import com.ni.vision.NIVision;
-import com.ni.vision.NIVision.Image;
+import org.opencv.core.Mat;
+import org.opencv.core.Point;
+import org.opencv.core.Scalar;
+import org.opencv.imgproc.Imgproc;
+
+import edu.wpi.cscore.CvSink;
+import edu.wpi.cscore.CvSource;
+import edu.wpi.cscore.UsbCamera;
+
+//import com.ni.vision.NIVision;
+//import com.ni.vision.NIVision.Image;
 
 import edu.wpi.first.wpilibj.CameraServer;
 import edu.wpi.first.wpilibj.Joystick;
@@ -15,17 +24,58 @@ public class DoubleCamera extends Subsystem {
 	private int camCenter;
 	private int camRight;
 	private int curCam;
-	private Image frame;
+	//private Image frame;
 	private CameraServer server;
+	private UsbCamera camera;
 	private Joystick stick;
 	boolean cameraStatus;
+	Thread visionThread;
     
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
 	public DoubleCamera(){
-		server = CameraServer.getInstance(); // single camera
-        server.setQuality(50);
-        server.startAutomaticCapture("cam0");
+		//server = CameraServer.getInstance(); // single camera
+        //server.setQuality(50);
+        //server.startAutomaticCapture("cam0");
+		
+		//CameraServer.getInstance().startAutomaticCapture();
+		
+		visionThread = new Thread(() -> {
+			// Get the UsbCamera from CameraServer
+			UsbCamera camera = CameraServer.getInstance().startAutomaticCapture();
+			// Set the resolution
+			camera.setResolution(480, 320);
+
+			// Get a CvSink. This will capture Mats from the camera
+			CvSink cvSink = CameraServer.getInstance().getVideo();
+			// Setup a CvSource. This will send images back to the Dashboard
+			CvSource outputStream = CameraServer.getInstance().putVideo("Rectangle", 480, 320);
+
+			// Mats are very memory expensive. Lets reuse this Mat.
+			Mat mat = new Mat();
+
+			// This cannot be 'true'. The program will never exit if it is. This
+			// lets the robot stop this thread when restarting robot code or
+			// deploying.
+			while (!Thread.interrupted()) {
+				// Tell the CvSink to grab a frame from the camera and put it
+				// in the source mat.  If there is an error notify the output.
+				if (cvSink.grabFrame(mat) == 0) {
+					// Send the output the error.
+					outputStream.notifyError(cvSink.getError());
+					// skip the rest of the current iteration
+					continue;
+				}
+				// Put a rectangle on the image
+				Imgproc.rectangle(mat, new Point(100, 100), new Point(400, 400),
+						new Scalar(255, 255, 255), 5);
+				// Give the output stream a new image to display
+				outputStream.putFrame(mat);
+			}
+		});
+		visionThread.setDaemon(true);
+		visionThread.start();
+		
 		
 //		stick = Robot.oi.getJoystick();
 //		// Get camera ids by supplying camera name ex 'cam0', found on roborio web interface
@@ -44,14 +94,14 @@ public class DoubleCamera extends Subsystem {
 	
 	public void StartCam0(){
 		server = CameraServer.getInstance();
-		server.setQuality(15);		
-		server.startAutomaticCapture("cam0");;
+		//server.setQuality(15);		
+		//server.startAutomaticCapture("cam0");;
 		
 	}
 	public void StartCam1(){
 		server = CameraServer.getInstance();
-		server.setQuality(15);		
-		server.startAutomaticCapture("cam1");
+		//server.setQuality(15);		
+		//server.startAutomaticCapture("cam1");
 	}
 
     public void initDefaultCommand() {
@@ -113,7 +163,7 @@ public class DoubleCamera extends Subsystem {
 	 */
 	public void end()
 	{
-		NIVision.IMAQdxStopAcquisition(curCam);
+		//NIVision.IMAQdxStopAcquisition(curCam);
 		
 	}
 	
@@ -123,9 +173,9 @@ public class DoubleCamera extends Subsystem {
 	 */
 	public void changeCam(int newId)
     {
-		NIVision.IMAQdxStopAcquisition(curCam);
-    	NIVision.IMAQdxConfigureGrab(newId);
-    	NIVision.IMAQdxStartAcquisition(newId);
+		//NIVision.IMAQdxStopAcquisition(curCam);
+    	//NIVision.IMAQdxConfigureGrab(newId);
+    	//NIVision.IMAQdxStartAcquisition(newId);
     	curCam = newId;
     }
     
@@ -134,8 +184,8 @@ public class DoubleCamera extends Subsystem {
 	 */
     public void updateCam()
     {
-    	NIVision.IMAQdxGrab(curCam, frame, 1);
-        server.setImage(frame);
+    	//NIVision.IMAQdxGrab(curCam, frame, 1);
+        //server.setImage(frame);
     }
 }
 
---
+++ src/org/usfirst/frc/team1635/robot/subsystems/VisionProcessing.java
@@ -0,0 +1,26 @@
+package org.usfirst.frc.team1635.robot.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ *
+ */
+public class VisionProcessing extends Subsystem {
+
+    // Put methods for controlling this subsystem
+    // here. Call these from Commands.
+	
+	public VisionProcessing(){
+		
+		
+		
+	}
+	
+	
+
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        //setDefaultCommand(new MySpecialCommand());
+    }
+}
+
